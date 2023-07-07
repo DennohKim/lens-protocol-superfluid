ba7b7f29c8b097f88d394dd608a31acd449f871b
Dennis Kimathi <Kimathidennis38@gmail.com>
2023-07-07 20:16:35 +0300

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,327 @@
-import Image from 'next/image'
+'use client';
+
+import React, { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
+import { Framework } from '@superfluid-finance/sdk-core';
+import { client, getProfiles, getFollowers } from '../../api';
+
+declare global {
+  interface Window {
+    ethereum?: any;
+  }
+}
+
+async function createNewFlow(recipient, flowRate) {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  await provider.send('eth_requestAccounts', []);
+  const signer = provider.getSigner();
+
+  const sf = await Framework.create({
+    chainId: 137,
+    provider: provider,
+  });
+  const superSigner = sf.createSigner({ signer: signer });
+  const maticx = await sf.loadSuperToken('MATICx');
+
+  console.log(maticx);
+
+  try {
+    const createFlowOperation = maticx.createFlow({
+      sender: await superSigner.getAddress(),
+      receiver: recipient,
+      flowRate: flowRate,
+    });
+
+    console.log(createFlowOperation);
+    console.log('Creating your stream...');
+
+    const result = await createFlowOperation.exec(superSigner);
+    console.log(result);
+
+    console.log(
+      `Congrats - you've just created a money stream!
+    `
+    );
+  } catch (error) {
+    console.log('Error: ', error);
+    console.error(error);
+  }
+}
 
 export default function Home() {
+  const [currentAccount, setCurrentAccount] = useState('');
+  const [profiles, setProfiles] = useState<any>([]);
+  const [flowRateDisplay, setFlowRateDisplay] = useState('');
+  const [flowRate, setFlowRate] = useState('');
+  const [recipient, setRecipient] = useState('');
+  const [viewType, setViewType] = useState('base-followers');
+  const [topProfiles, setTopProfiles] = useState<any>([]);
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+  async function fetchTopFollowers(account) {
+    try {
+      const profileId = await fetchProfileId(account);
+      const data = await fetch('/api/lens-bigquery', {
+        method: 'POST',
+        body: JSON.stringify({
+          profileId,
+        }),
+      });
+      const json = await data.json();
+      if (json.data) {
+        setTopProfiles(json.data);
+      }
+    } catch (err) {
+      console.log('error fetching top followers...', err);
+    }
+  }
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        alert('Get MetaMask!');
+        return;
+      }
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+      console.log('Connected', accounts[0]);
+      setCurrentAccount(accounts[0]);
+      fetchFollowers(accounts[0]);
+      fetchTopFollowers(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log('Make sure you have metamask!');
+      return;
+    } else {
+      console.log('We have the ethereum object', ethereum);
+    }
+
+    const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log('Found an authorized account:', account);
+      setCurrentAccount(account);
+      fetchFollowers(account);
+      fetchTopFollowers(account);
+    } else {
+      console.log('No authorized account found');
+    }
+  };
+
+  async function fetchProfileId(account) {
+    try {
+      let response = await client.query({
+        query: getProfiles,
+        variables: {
+          addresses: [account],
+        },
+      });
+
+      return response.data.profiles.items[0].id;
+    } catch (err) {
+      console.log('error fetching profile id...', err);
+    }
+  }
+
+  async function fetchFollowers(account) {
+    try {
+      const profileId = await fetchProfileId(account);
+      const response = await client.query({
+        query: getFollowers,
+        variables: {
+          profileId,
+        },
+      });
+      let profileData = response.data.followers.items.filter((profile) => {
+        if (profile.wallet.defaultProfile) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+      profileData = profileData
+        .map((p) => {
+          return {
+            ...p.wallet.defaultProfile,
+            address: p.wallet.address,
+          };
+        })
+        .filter((p) => p.picture);
+      console.log('profileData:', profileData);
+      setProfiles(profileData);
+    } catch (err) {
+      console.log('error getting followers:', err);
+    }
+  }
+
+  const handleRecipientChange = (e) => {
+    setRecipient(() => ([e.target.name] = e.target.value));
+  };
+
+  const handleFlowRateChange = (e) => {
+    setFlowRate(() => ([e.target.name] = e.target.value));
+    let newFlowRateDisplay = calculateFlowRate(e.target.value);
+    if (newFlowRateDisplay) {
+      setFlowRateDisplay(newFlowRateDisplay.toString());
+    }
+  };
+
+  function calculateFlowRate(amount) {
+    if (Number(amount) === 0) {
+      return 0;
+    }
+    const amountInWei = ethers.BigNumber.from(amount);
+    const monthlyAmount = ethers.utils.formatEther(amountInWei.toString());
+    // @ts-ignore
+    const calculatedFlowRate = monthlyAmount * 3600 * 24 * 30;
+    return calculatedFlowRate;
+  }
+
+  const topFollowers = viewType === 'top-followers';
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <main className='flex flex-col justify-between p-24'>
+      <h1>Superfluid X Lens</h1>
+      {!currentAccount && (
+        <button
+          onClick={connectWallet}
+          className='mb-3 px-8 py-2 rounded-3xl bg-white text-black mt-2'
+        >
+          Connect Wallet
+        </button>
+      )}
+      {currentAccount && <p className='mt-3 mb-3'>{currentAccount}</p>}
+      <div className='flex flex-col items-start'>
+        <input
+          value={recipient}
+          placeholder='Enter recipient address'
+          onChange={handleRecipientChange}
+          className='text-black py-1 px-2 mb-2 w-72'
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <input
+          value={flowRate}
+          onChange={handleFlowRateChange}
+          placeholder='Enter a flowRate in wei/second'
+          className='text-black py-1  px-2 w-72'
+        />
+        <button
+          className='px-8 py-2 rounded-3xl bg-white text-black mt-2'
+          onClick={() => {
+            createNewFlow(recipient, flowRate);
+          }}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
+          Click to Create Your Stream
+        </button>
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='mt-4 text-green-400'
+          href='https://app.superfluid.finance/'
+          target='_blank'
+          rel='no-opener'
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+          View Superfluid Dashboard
         </a>
       </div>
+      <div className='border-green-400 border p-4 mt-3'>
+        <p>Your flow will be equal to:</p>
+        <p>
+          <b>${flowRateDisplay !== ' ' ? flowRateDisplay : 0}</b> Maticx/month
+        </p>
+      </div>
+      <div className='mt-3 mb-3'>
+        <button
+          onClick={() => setViewType('base-followers')}
+          className={`${
+            !topFollowers ? 'bg-green-400' : 'bg-gray-200'
+          } py-2 px-10 text-black`}
+        >
+          All Followers
+        </button>
+        <button
+          onClick={() => setViewType('top-followers')}
+          className={`${
+            topFollowers ? 'bg-green-400' : 'bg-gray-200'
+          } py-2 px-10 ml-1 text-black`}
+        >
+          Top Followers
+        </button>
+      </div>
+      {viewType === 'base-followers' &&
+        profiles.map((profile, index) => (
+          <div
+            key={index}
+            className='
+            p-3 border-white border mt-4 border-slate-400	cursor-pointer
+          '
+            onClick={() => setRecipient(profile.address)}
+          >
+            {profile.picture?.original?.url && (
+              <img
+                className='w-32 rounded-2xl'
+                src={getGateway(profile.picture?.original?.url)}
+              />
+            )}
+            {profile.picture?.url && (
+              <img
+                className='w-32 rounded-2xl'
+                src={getGateway(profile.picture.url)}
+              />
+            )}
+            {profile.picture?.uri && (
+              <img
+                className='w-32 rounded-2xl'
+                src={getGateway(profile.picture.uri)}
+              />
+            )}
+            <p className='mt-2 text-xl text-fuchsia-400'>@{profile.handle}</p>
+            <p>{profile.name}</p>
+          </div>
+        ))}
+      {viewType === 'top-followers' &&
+        topProfiles.map((profile, index) => (
+          <div
+            key={index}
+            className='
+          p-3 border-white border mt-4 border-slate-400	cursor-pointer
+        '
+            onClick={() => setRecipient(profile.owned_by)}
+          >
+            <img
+              className='w-32 rounded-2xl'
+              src={getGateway(profile.profile_picture_s3_url)}
+            />
+            <p className='mt-2 text-xl text-fuchsia-400'>@{profile.handle}</p>
+            <p>{profile.name}</p>
+          </div>
+        ))}
     </main>
-  )
+  );
+}
+
+function getGateway(hashoruri) {
+  if (hashoruri.includes('https')) {
+    return hashoruri;
+  }
+  if (hashoruri.includes('ipfs://')) {
+    console.log(
+      'ipfs: ',
+      hashoruri.replace('ipfs://', 'https://ipfs.io/ipfs/')
+    );
+    return hashoruri.replace('ipfs://', 'https://ipfs.io/ipfs/');
+  }
+  if (hashoruri.includes('ar://')) {
+    console.log('ar: ', hashoruri.replace('ar://', 'https://arweave.net/'));
+    return hashoruri.replace('ar://', 'https://arweave.net/');
+  }
 }
